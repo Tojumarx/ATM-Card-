74a7de6d31b8c5b0c5d71ccd3309ff837ef56bab
Marx Toritseju <tojumarx@gmail.com>
2024-07-29 16:57:44 +0100

    done with assignment 2

---
+++ ATM Card.js
@@ -0,0 +1,7 @@
+function atmDetails(){
+    const atmNumber = "1234 5678 9098 7654", atmName = "Toju Marx", atmExpdate = "12/26", atmCVV = "246";
+    document.getElementById("atmNumber").textContent = atmNumber;
+    document.getElementById("atmName").textContent = atmName;
+    document.getElementById("atmExpdate").textContent = atmExpdate;
+    document.getElementById("atmCVV").textContent = atmCVV;
+}
